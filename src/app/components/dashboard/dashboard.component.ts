5ea5be86968ef233cfc2439d89b053c54e504463
Avraham Ahituv <atahituv@gmail.com>
2020-04-12 00:07:38 +0300

    update

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -10,28 +10,27 @@ import { faPizzaSlice, faTorah, faAddressBook, faCalendarAlt } from '@fortawesom
 })
 export class DashboardComponent  {
 
-  
   /** Based on the screen size, switch from standard to one column per row */
   cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
     map(({ matches }) => {
       if (matches) {
         return [
-          { title: 'מתכונים', cols: 1, rows: 1, link: 'recipes', icon: faPizzaSlice },
-          { title: 'אנשי קשר', cols: 1, rows: 1, link: 'contacts', icon: faAddressBook },
-          { title: 'לוח שנה', cols: 1, rows: 1, link: 'calendar', icon: faCalendarAlt },
+          { title: 'מתכונים', cols: 1, rows: 1, link: 'recipes', icon: faPizzaSlice , classes: 'card-back-1'},
+          { title: 'אנשי קשר', cols: 1, rows: 1, link: 'contacts', icon: faAddressBook, classes: 'card-back-2'},
+          { title: 'לוח שנה', cols: 1, rows: 1, link: 'calendar', icon: faCalendarAlt, classes: 'card-back-3' },
           // { title: 'רשימת קניות', cols: 1, rows: 1, link:'',content:'מוצרים אחרונים' },
           // { title: 'מוזיקה', cols: 1, rows: 1, link:'music' },
-          { title: 'שיעורי תורה', cols: 1, rows: 1, link: 'lessons', icon: faTorah }
+          { title: 'שיעורי תורה', cols: 1, rows: 1, link: 'lessons', icon: faTorah, classes: 'card-back-4' }
         ];
       }
 
       return [
-        { title: 'מתכונים', cols: 1, rows: 1, link: 'recipes', icon: faPizzaSlice },
-        { title: 'אנשי קשר', cols: 1, rows: 1, link: 'contacts', icon: faAddressBook },
-        { title: 'לוח שנה', cols: 1, rows: 1, link: 'calendar', icon: faCalendarAlt },
+        { title: 'מתכונים', cols: 1, rows: 1, link: 'recipes', icon: faPizzaSlice , classes: 'card-back-1'},
+        { title: 'אנשי קשר', cols: 1, rows: 1, link: 'contacts', icon: faAddressBook, classes: 'card-back-2' },
+        { title: 'לוח שנה', cols: 1, rows: 1, link: 'calendar', icon: faCalendarAlt , classes: 'card-back-3'},
         // { title: 'רשימת קניות', cols: 1, rows: 1, link:'',content:'מוצרים אחרונים' },
         // { title: 'מוזיקה', cols: 1, rows: 1, link:'music' },
-        { title: 'שיעורי תורה', cols: 1, rows: 1, link: 'lessons', icon: faTorah }
+        { title: 'שיעורי תורה', cols: 1, rows: 1, link: 'lessons', icon: faTorah, classes: 'card-back-4' }
       ];
     })
   );
@@ -39,5 +38,5 @@ export class DashboardComponent  {
   constructor(private breakpointObserver: BreakpointObserver) {
 
   }
-  
+
 }
